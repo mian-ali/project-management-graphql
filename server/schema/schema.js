701b7d05f2f9ef21bfe7f6842f9151102a822537
Ali Ahmad <mian41935@gmail.com>
2022-06-23 22:53:15 +0500

    ClientTypes and ProjectType Queries graphql

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -5,7 +5,21 @@ const {
   GraphQLID,
   GraphQLString,
   GraphQLSchema,
+  GraphQLList,
 } = require('graphql');
+
+//Projects types
+
+const ProjectType = new GraphQLObjectType({
+  name: 'Project',
+
+  fields: () => ({
+    id: { type: GraphQLID },
+    name: { type: GraphQLID },
+    description: { type: GraphQLID },
+    status: { type: GraphQLID },
+  }),
+});
 
 //Clients types
 
@@ -23,6 +37,26 @@ const ClientType = new GraphQLObjectType({
 const RootQuery = new GraphQLObjectType({
   name: 'RootQueryType',
   fields: {
+    projects: {
+      type: new GraphQLList(ProjectType),
+      resolve(parent, args) {
+        return projects;
+      },
+    },
+    project: {
+      type: ProjectType,
+      args: { id: { type: GraphQLID } },
+      resolve(parent, args) {
+        return projects.find((project) => project.id === args.id);
+      },
+    },
+
+    clients: {
+      type: new GraphQLList(ClientType),
+      resolve(parent, args) {
+        return clients;
+      },
+    },
     client: {
       type: ClientType,
       args: { id: { type: GraphQLID } },
